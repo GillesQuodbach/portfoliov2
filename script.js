ad20a536a0244f25d613e361e8a0e30be0cfac28
Gilles QUODBACH <quodbach.gilles@gmail.com>
2023-10-16 11:29:33 +0200

    slider implementation

---
+++ script.js
@@ -0,0 +1,81 @@
+//Slider
+
+const slider = function () {
+  const slides = document.querySelectorAll(".slide");
+  const btnLeft = document.querySelector(".slider_btn-left");
+  const btnRight = document.querySelector(".slider_btn-right");
+  const dotContainer = document.querySelector(".dots");
+
+  let currentSlide = 0;
+  const maxSlide = slides.length;
+
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots_dot" data-slide="${i}"</button>`
+      );
+    });
+  };
+
+  const activeDot = function (slide) {
+    document
+      .querySelectorAll(".dots_dot")
+      .forEach((dot) => dot.classList.remove("dots_dot-active"));
+    document
+      .querySelector(`.dots_dot[data-slide="${slide}"]`)
+      .classList.add("dots_dot-active");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  const nextSlide = function () {
+    if (currentSlide === maxSlide - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+    goToSlide(currentSlide);
+    activeDot(currentSlide);
+  };
+
+  const prevSlide = function () {
+    if (currentSlide === 0) {
+      currentSlide = maxSlide - 1;
+    } else {
+      currentSlide--;
+    }
+    goToSlide(currentSlide);
+    activeDot(currentSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+    activeDot(0);
+  };
+
+  init();
+
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots_dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activeDot(currentSlide);
+    }
+  });
+};
+
+slider();
