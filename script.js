0cb4627ebe35b8ddec4c5e683c8f03f6110ec877
Gilles QUODBACH <quodbach.gilles@gmail.com>
2023-10-16 12:38:35 +0200

    maj scroll

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-//Slider
+"use strict";
 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn-close-modal");
+const btnOpenModal = document.querySelectorAll(".btn-show-modal");
+const btnScrollTo = document.querySelector(".btn-scroll-to");
+const sectionHome = document.querySelector("#section-home");
+const sectionAboutMe = document.querySelector("#section-aboutme");
+const sectionProjects = document.querySelector("#section-projects");
+const sectionContact = document.querySelector("#section-contact");
+const nav = document.querySelector(".nav");
+
+//LEARN MORE BUTTON
+btnScrollTo.addEventListener("click", function (e) {
+  const sectionAboutMeCoords = sectionAboutMe.getBoundingClientRect();
+  sectionAboutMe.scrollIntoView({ behavior: "smooth" });
+});
+
+//Modal
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+//Slider
 const slider = function () {
   const slides = document.querySelectorAll(".slide");
   const btnLeft = document.querySelector(".slider_btn-left");
